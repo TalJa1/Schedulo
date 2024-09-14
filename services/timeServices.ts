e03d9af67398ea75230894dfebd8634d0a575b36
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-14 13:26:21 +0700

    Refactor timeServices to add getCurrentWeekDays function

--- services/timeServices.ts
+++ services/timeServices.ts
@@ -4,3 +4,38 @@ export const getCurrentMonthAndYear = () => {
   const year = currentDate.getFullYear().toString();
   return `Tháng ${month}.${year}`;
 };
+
+export const getCurrentWeekDays = () => {
+  const currentDate = new Date();
+  const currentDay = currentDate.getDay();
+  const startOfWeek = new Date(currentDate);
+  const endOfWeek = new Date(currentDate);
+
+  // Adjust to the start of the week (Monday)
+  startOfWeek.setDate(
+    currentDate.getDate() - (currentDay === 0 ? 6 : currentDay - 1),
+  );
+  // Adjust to the end of the week (Sunday)
+  endOfWeek.setDate(startOfWeek.getDate() + 6);
+
+  const daysOfWeek = [
+    'Chủ Nhật',
+    'Thứ 2',
+    'Thứ 3',
+    'Thứ 4',
+    'Thứ 5',
+    'Thứ 6',
+    'Thứ 7',
+  ];
+  const weekDays = [];
+
+  for (let i = 0; i < 7; i++) {
+    const day = new Date(startOfWeek);
+    day.setDate(startOfWeek.getDate() + i);
+    const dayOfWeek = daysOfWeek[day.getDay()];
+    const dayOfMonth = day.getDate().toString().padStart(2, '0');
+    weekDays.push({dayOfWeek, dayOfMonth});
+  }
+
+  return weekDays;
+};
