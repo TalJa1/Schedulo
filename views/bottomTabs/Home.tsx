8b17e1a5b31b056348be51e69e9c6b9d7ba092e4
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-14 15:29:43 +0700

    Refactor Home component to add tab navigation for different sections

--- views/bottomTabs/Home.tsx
+++ views/bottomTabs/Home.tsx
@@ -1,7 +1,14 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {containerStyle} from '../../services/styleSheet';
+import {containerStyle, vh, vw} from '../../services/styleSheet';
 import useStatusBar from '../../services/useStatusBarCustom';
 import HeaderComponent from '../../components/home/HeaderComponent';
 import {getTodayIndex} from '../../services/timeServices';
@@ -9,6 +16,8 @@ import {getTodayIndex} from '../../services/timeServices';
 const Home = () => {
   useStatusBar('#363851');
   const [weekDayIndex, setWeekDayIndex] = useState(getTodayIndex());
+  const [tabCurrent, setTabCurrent] = useState(0);
+  const tabs = ['Việc cần làm', 'Thử thách', 'Thời gian biểu'];
 
   return (
     <SafeAreaView style={styles.container}>
@@ -18,7 +27,33 @@ const Home = () => {
             dayIndex={weekDayIndex}
             setDayIndex={setWeekDayIndex}
           />
-          <Text>Home</Text>
+          <View
+            style={{
+              paddingHorizontal: vw(5),
+              flexDirection: 'row',
+              columnGap: vw(2),
+              marginVertical: vh(1),
+            }}>
+            {tabs.map((tab, index) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => setTabCurrent(index)}
+                  style={[
+                    styles.tabBtn,
+                    index === tabCurrent && {backgroundColor: '#363851'},
+                  ]}
+                  key={index}>
+                  <Text
+                    style={[
+                      styles.tabTxt,
+                      index === tabCurrent && {color: 'white'},
+                    ]}>
+                    {tab}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -29,4 +64,15 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: containerStyle,
+  tabTxt: {
+    color: '#363851',
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  tabBtn: {
+    borderWidth: 1,
+    borderColor: '#363851',
+    borderRadius: vw(20),
+    padding: vw(2),
+  },
 });
