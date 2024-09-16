71d90bd6ce7aaa9b3e12210a7b356202f7672c5c
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-16 13:51:52 +0700

    Refactor svgXML.tsx to add color parameter to homeNotiIcon component

--- views/bottomTabs/StoragePage.tsx
+++ views/bottomTabs/StoragePage.tsx
@@ -1,14 +1,16 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import React from 'react';
-import {containerStyle} from '../../services/styleSheet';
+import {containerStyle, vh, vw} from '../../services/styleSheet';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBarCustom';
+import {homeNotiIcon, searchIcon} from '../../assets/svgXML';
 
 const StoragePage = () => {
-  useStatusBar('#363851');
+  useStatusBar('#F9FAFF');
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
+        <Header />
         <View>
           <Text>StoragePage</Text>
         </View>
@@ -17,8 +19,63 @@ const StoragePage = () => {
   );
 };
 
+const Header: React.FC = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+  };
+  return (
+    <View style={styles.headerContainer}>
+      <View style={styles.searchBarContainer}>
+        {searchIcon(vw(4), vw(4), '#1940B6')}
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Tìm kiếm tài liệu"
+          placeholderTextColor={'#A4A2A2'}
+          value={searchQuery}
+          onChangeText={handleSearch}
+        />
+      </View>
+      {homeNotiIcon(vw(7), vw(7), '#1940B6')}
+    </View>
+  );
+};
+
 export default StoragePage;
 
 const styles = StyleSheet.create({
   container: containerStyle,
+  headerStyle: {
+    backgroundColor: '#F9FAFF',
+    paddingVertical: vh(2),
+  },
+  headerContainer: {
+    padding: 10,
+    backgroundColor: '#F9FAFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchBar: {
+    flex: 1,
+    height: 40,
+    shadowRadius: 8,
+    borderColor: '#FFFFFF',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+    paddingLeft: 10,
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    flex: 1,
+    marginRight: 10,
+  },
 });
