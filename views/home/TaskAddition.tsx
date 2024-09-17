196d8f2477c0a245d9a38a5aa454bcb05e48a659
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-17 09:58:10 +0700

    Refactor svgXML.tsx and TaskAddition.tsx to add date picker icon and input fields for task addition

--- views/home/TaskAddition.tsx
+++ views/home/TaskAddition.tsx
@@ -1,7 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import React from 'react';
 import useStatusBar from '../../services/useStatusBarCustom';
 import TaskAdditionComponent from '../../components/home/TaskAdditionComponent';
+import {vh, vw} from '../../services/styleSheet';
+import {datePickerIcon} from '../../assets/svgXML';
 
 const TaskAddition = () => {
   useStatusBar('#1940B6');
@@ -12,10 +15,53 @@ const TaskAddition = () => {
   );
 };
 
-const MainInput: React.FC = () => {
+const TextInputGroup: React.FC<{
+  title: string;
+  placeholder: string;
+}> = ({placeholder, title}) => {
   return (
     <View>
-      <Text>TaskAddition</Text>
+      <Text style={{color: '#FFFFFF', fontSize: 20, fontWeight: '700'}}>
+        {title}
+      </Text>
+      <TextInput
+        style={{
+          borderBottomColor: '#D2D2D2',
+          borderBottomWidth: 1,
+          borderColor: '#D2D2D2',
+        }}
+        placeholder={placeholder}
+        placeholderTextColor={'#FFFFFF4D'}
+      />
+    </View>
+  );
+};
+
+const MainInput: React.FC = () => {
+  return (
+    <View style={{paddingHorizontal: vw(5), rowGap: vh(2)}}>
+      <TextInputGroup placeholder="Điền tiêu đề" title="Tiêu đề" />
+      <TextInputGroup placeholder="Điền ghi chú" title="Ghi chú" />
+      <View>
+        <Text style={{color: '#FFFFFF', fontSize: 20, fontWeight: '700'}}>
+          Chọn ngày
+        </Text>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TextInput
+            style={{
+              flex: 1,
+              borderBottomColor: '#D2D2D2',
+              borderBottomWidth: 1,
+              borderColor: '#D2D2D2',
+            }}
+            placeholder={'Chọn 1 ngày'}
+            placeholderTextColor={'#FFFFFF4D'}
+          />
+          <View style={{position: 'absolute', right: 0}}>
+            {datePickerIcon(vw(7), vw(7))}
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
