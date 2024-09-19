10ef25864b4df8b83797faecbb3fd1b138763bdb
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-09-19 15:36:33 +0700

    Refactor Challenge component to update rendering logic based on challenge data and add checkbox functionality

--- components/home/Challenge.tsx
+++ components/home/Challenge.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
 import {Image, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {ChallengeComponentProps} from '../../services/typeProps';
+import React, {useState} from 'react';
+import {ChallengeComponentProps, ChallengeItem} from '../../services/typeProps';
 import {vw} from '../../services/styleSheet';
+import CheckBox from '@react-native-community/checkbox';
 
 const Challenge: React.FC<ChallengeComponentProps> = ({
   handleNavigate,
@@ -23,7 +24,10 @@ const Challenge: React.FC<ChallengeComponentProps> = ({
   return (
     <View style={styles.container}>
       {todayIndex > weekDayIndex ? (
-        <PastDayView />
+        <PastDayView
+          data={challengeData[weekDayIndex]}
+          isToday={todayIndex === weekDayIndex}
+        />
       ) : challengeData[weekDayIndex] &&
         challengeData[weekDayIndex][0] &&
         challengeData[weekDayIndex][0].title === '' ? (
@@ -51,24 +55,62 @@ const ContentView: React.FC = () => {
   );
 };
 
-const PastDayView: React.FC = () => {
+const PastDayView: React.FC<{data: ChallengeItem[]; isToday: boolean}> = ({
+  data,
+  isToday,
+}) => {
+  const [checkedTasks, setCheckedTasks] = useState<{[index: number]: boolean}>(
+    {},
+  );
+
+  const handleCheck = (index: number) => {
+    setCheckedTasks(prev => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
+
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        width: '100%',
-        alignItems: 'center',
-        paddingHorizontal: vw(5),
-        columnGap: vw(5),
-      }}>
-      <Image
-        style={{width: vw(30), height: vw(30), resizeMode: 'contain'}}
-        source={require('../../assets/home/noContent.png')}
-      />
-      <Text
-        style={{color: '#1940B6', fontSize: 20, fontWeight: '700', flex: 1}}>
-        Tốt lắm! Bạn đã hoàn thành thử thách!
-      </Text>
+    <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '100%',
+          alignItems: 'center',
+          paddingHorizontal: vw(5),
+          columnGap: vw(5),
+        }}>
+        <Image
+          style={{width: vw(30), height: vw(30), resizeMode: 'contain'}}
+          source={require('../../assets/home/noContent.png')}
+        />
+        <Text
+          style={{color: '#1940B6', fontSize: 20, fontWeight: '700', flex: 1}}>
+          Tốt lắm! Bạn đã hoàn thành thử thách!
+        </Text>
+      </View>
+      <View style={{paddingHorizontal: vw(5)}}>
+        {data[0].title !== '' && (
+          <>
+            {data.map((value, index) => {
+              return (
+                <View key={index}>
+                  <CheckBox
+                    disabled={!isToday}
+                    tintColors={{true: '#1940B6', false: '#D3D3D3'}}
+                    value={checkedTasks[index] || false}
+                    onValueChange={() => handleCheck(index)}
+                  />
+                  <View>
+                    <Text>{value.title}</Text>
+                    <Text>{value.aim}</Text>
+                  </View>
+                </View>
+              );
+            })}
+          </>
+        )}
+      </View>
     </View>
   );
 };
